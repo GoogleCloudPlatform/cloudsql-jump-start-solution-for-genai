fec3661cb1ad5f8439d5c1f64cef405f55554ba2
jackwotherspoon <jackwoth@google.com>
2024-05-15 00:15:25 +0000

    chore: update k8s jobs to Cloud Run jobs

--- scripts/configure-jobs.sh
+++ scripts/configure-jobs.sh
@@ -17,12 +17,14 @@
 import sys
 
 
-def prepare_template(filename, project_name):
+def prepare_template(filename, project_name, region="us-central1"):
     lines = []
     with open(filename, "r") as fin:
         for line in fin:
             if "__PROJECT__" in line:
                 line = line.replace("__PROJECT__", project_name)
+            if "__REGION__" in line:
+                line = line.replace("__REGION__", region)
             lines.append(line)
     with open(filename, "w") as fout:
         fout.writelines(lines)
@@ -30,9 +32,12 @@ def prepare_template(filename, project_name):
 
 yaml_templates = [
     "chatbot-api/k8s/deployment.yaml",
-    "init-db/k8s/job.yaml",
-    "load-embeddings/k8s/job.yaml",
+    "init-db/job.yaml",
+    "load-embeddings/job.yaml",
 ]
 
 for t in yaml_templates:
-    prepare_template(t, sys.argv[1])
+    if len(sys.argv) == 3:
+        prepare_template(t, sys.argv[1], sys.argv[2])
+    else:
+        prepare_template(t, sys.argv[1])

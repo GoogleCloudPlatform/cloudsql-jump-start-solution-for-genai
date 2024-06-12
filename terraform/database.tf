95901f88c8138185ebe64219c8688c3df99773a7
jackwotherspoon <jackwoth@google.com>
2024-06-12 21:11:04 +0000

    feat: use a Postgres 16 instance

--- terraform/database.tf
+++ terraform/database.tf
@@ -20,7 +20,7 @@ resource "random_password" "default" {
 
 resource "google_sql_database_instance" "default" {
   project          = var.google_cloud_db_project
-  database_version = "POSTGRES_15"
+  database_version = "POSTGRES_16"
   name             = "toys-inventory"
   region           = var.google_cloud_default_region
   root_password    = random_password.default.result

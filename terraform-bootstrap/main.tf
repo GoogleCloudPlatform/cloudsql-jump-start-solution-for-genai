d7858309d24aa65fb36ace2c90121c4736d272ce
Jack Wotherspoon <jackwoth@google.com>
2024-06-13 11:58:02 -0400

    chore: update tf bootstrap

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -26,7 +26,7 @@ resource "google_project_service" "default" {
 
 resource "google_storage_bucket" "default" {
   name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
+  force_destroy = true
   location      = "US"
   storage_class = "STANDARD"
 
